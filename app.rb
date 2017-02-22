7379c2a57e21944d2ab4382c59c507d4c85712b7
doverh <doverheld@gmail.com>
2017-02-22 14:33:03 -0500

    Adding testing app

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+class PersonalDetailsApp < Sinatra::Base
+	get '/' do
+		erb:name
+	end
+
+	post '/name' do
+		name = params[:name]
+		redirect 'age??user_name'+ name
+	end
+	get '/age' do
+		name = params[:user_name]
+	end
+
+end
---
+++ tests/test_app.rb
@@ -0,0 +1,35 @@
+require "minitest/autorun"
+require "rack/test"
+require_relative'../app.rb'
+
+class TestApp < Minitest::Test
+	include Rack::Test::Methods
+
+	def app
+		PersonalDetailsApp
+	end	
+
+	def test_get_test_entry_page
+		get '/'
+		assert(last_response.ok?)
+		assert(last_response.body.include?('Hello, whats your name?'))
+		assert(last_response.body.include?('<input type "text" name="name">'))
+		assert(last_response.body.include?('<form method="post" action ="name">'))
+	
+	end
+
+	def test_post_name
+		post '/name', name:'Jon'
+		follow_redirect!
+		assert(last_response.ok?)
+		assert(last_response.body.include?('Jon'))
+		
+	end
+
+	def test_get_age
+		get '/age?user_name = Dover'
+		assert(last_response.ok?)
+		assert(last_response.body.include?('Dover'))
+	end
+
+end
